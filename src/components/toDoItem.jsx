0bdca2736338f3cfa37abc809b3be5d43a781fa0
Anthony Holiday <138096306+anthonyholiday@users.noreply.github.com>
2025-07-08 10:01:24 +0200

    Added toDoList state and rendered to the DOM

--- src/components/toDoItem.jsx
+++ src/components/toDoItem.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 
-export default function Item()  {
+export default function Item(props)  {
     return (
-        <li className="px-8 py-4 bg-slate-700">List Item goes here</li>
+        <li className="px-8 py-4 bg-slate-700 flex flex-row justify-between w-100">
+            <p>{props.title}</p>
+            <a>Delete</a>
+        </li>
     )
 }
--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -2,9 +2,24 @@ import React from "react"
 import Item from "./toDoItem.jsx"
 
 export default function ToDoList(props)  {
+    const [toDoList, setToDoList] = React.useState([
+        {
+            title: "this is an item"
+        },
+        {
+            title: "this is another item #2"
+        },
+        {
+            title: "this is #3"
+        }
+    ])
     return (
-        <ul>
-            <Item />
+        <ul className="flex flex-col gap-4">
+            {
+                toDoList.map(item => {
+                return <Item title={item.title} />
+                })
+            }
         </ul>
     )
 }
