a52c994c6b0c3d258f0d66175d781bd81d035b1e
Anthony Holiday <138096306+anthonyholiday@users.noreply.github.com>
2025-07-15 18:52:11 +0100

    Added border to image

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Image(props) {
-    return <img src={props.image} className="rounded rounded-full w-36 h-36 lg:w-56 lg:h-56 object-cover shadow-md"/>
+    return <img src={props.image} className="rounded rounded-full w-36 h-36 lg:w-56 lg:h-56 object-cover shadow-md border border-slate-800"/>
 }
